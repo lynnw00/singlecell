0c5be3a95b02979403ccc9094c78204c0cd644b2
lynnw00 <lynnwachelder@gmail.com>
2026-02-06 15:29:10 +0100

    Update featureCounts.sh

--- featureCounts.sh
+++ featureCounts.sh
@@ -24,6 +24,7 @@ outDir="$path/results/featureCounts"
 # I want to use -p for paired ends and counts only 1 read per template
 # I want to count only primary alignments
 # I believe this is a reverse stranded library
+# I want to break free
 
 echo "Started job at $(date)"
 mkdir -p $outDir
@@ -31,3 +32,13 @@ mkdir -p $outDir
 featureCounts -p --countReadPairs -C -T 4 -s 2 --primary \
 	-a $gtfFile -o $outDir/counts.tsv $bamfile
 echo "Job finished at $(date)"
+
+# Uses the aligned reads, the BAM file created with STAR.
+# The GTF file contains the gene annotations.
+# -p, it is paired-end data, looking at fragments and not individual reads.
+# countReadPairs one unit per read pair, not two reads.
+# -C checks for pair consistency. -T is the threads.
+# -s is the strandedness, the data is stranded, reverse strand.
+# Only counting primary alignments.
+# -a is the annotation file, defining genes, transcripts and exons.
+# The counts.tsv will contain gene ID, length and number of read pairs assigned to each gene.
