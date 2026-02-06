a31809eabaddf9eb76ce24dc21d678ed460b1fc8
lynnw00 <lynnwachelder@gmail.com>
2026-02-06 15:56:19 +0100

    Update emapper.sh

--- emapper.sh
+++ emapper.sh
@@ -26,3 +26,13 @@ emapper.py -i ${path}/proteins.fasta --go_evidence experimental --output_dir ${o
 --cpu 16 --itype proteins -m diamond --evalue 1e-5 --temp_dir ${path}/tempDir
 
 echo "Job finished at $(date)"
+
+# Input is the protein FASTA file.
+# go_evidence only assigns GO terms with experimental evidence, not predicted ones. This
+# produces high-confidence functional annotations.
+# -o is again the prefix for the output file.
+# Using 16 CPU threads because EggNOG can parallelize searches.
+# -m diamond is a search method, it is a fast sequence alignment method.
+# evalue sets a threshold, only hits with e-value < 1e-5 are considered, this filters out
+# weak homology matches.
+# temp_dir for storing temporary files in this directory.
